56212b0b63a6c7379d738d7472f0d9fd759d1614
SergioGarciaPizarro <120561905+SergioGarciaPizarro@users.noreply.github.com>
2024-01-09 19:35:50 +0100

    Cambio de js y evento click
    Probamos los eventos click para cada provincia y tratamos de mandar por URL el id de la provincia al otro js

--- JS/index.js
+++ JS/index.js
@@ -7,41 +7,11 @@ indexDBStatic.indexDBProvinceExists().then(result => console.log(result));
 utilities.getInfoFromClient().then(data=>console.log(data))
 */
 
-const URLMunicipios = `https://www.el-tiempo.net/api/json/v2/provincias/`; // hay que añadir el CODPROV de la provincia seleccionada en el mapa
-const finalURLMunicipios = `/municipios`; // se le añade al final de la url municipios una vez se tenga el CODPROV
-const masInfoURL = `https://www.el-tiempo.net/api/json/v2/provincias/06/municipios/06083` // mas info merida
-
-let informacion = {};
-
-let arrayObjMunicipio = [];
-
-let URLFinal = `${URLMunicipios}06${finalURLMunicipios}`;
-
-fetch(URLFinal)
-    .then(response => response.json())
-    .then(datosProv => {
-        console.log(datosProv);
-
-        const nombre = datosProv.provincia;
-        const idProv = datosProv.codprov;
-
-        informacion.nombre = nombre;
-        informacion.idProv = idProv;
-
-        datosProv.municipios.forEach(elemento => {
-            let nombreMunicipio = elemento.NOMBRE;
-            let IDMunicioio = elemento.CODIGOINE.substr(0, 5)
-            let poblacionMunicipio = elemento.POBLACION_MUNI;
-           
-            let obj = { "nombreMun": nombreMunicipio, "IDMun": IDMunicioio, "poblacionMun": poblacionMunicipio};
-            arrayObjMunicipio.push(obj);
-        });
-        informacion.arrayObjMunicipio = arrayObjMunicipio;
-        console.log(informacion)
-    })
-    .catch(error => {
-        console.error("Error en la solicitud:", error);
+Array.from(document.querySelectorAll(".provincias")).forEach((e)=>{
+    e.addEventListener("click", ()=>{
+      window.location.href = `./PAGES/municipios.html?id=${e.id}`;
     });
+  });
 
 
 
