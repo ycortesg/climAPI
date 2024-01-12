a3f28328ab6b6426a91cfa1c7ac5509020fed85e
SergioGarciaPizarro <120561905+SergioGarciaPizarro@users.noreply.github.com>
2024-01-12 17:01:45 +0100

    Corrección

--- JS/municipios.js
+++ JS/municipios.js
@@ -12,6 +12,8 @@ const lista = document.getElementById("lista");
 let informacion;
 let arrayObjMunicipio = [];
 
+let URLFinal = `${URLMunicipios}${idProvinciaURL}${finalURLMunicipios}`
+
 async function indexDBInicicial() {
   await indexDBStatic.openDB();
   informacion = await indexDBStatic.getPriovinceDB(idProvinciaURL);
