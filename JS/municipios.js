6c283beb242bce4634a035e9bb5fa46072dbe8cc
SergioGarciaPizarro <120561905+SergioGarciaPizarro@users.noreply.github.com>
2024-01-14 11:42:39 +0100

    Hover
    Ponerle un Hover a los municipios

--- JS/municipios.js
+++ JS/municipios.js
@@ -54,7 +54,7 @@ async function indexDBInicicial() {
     let div = document.createElement("div");
     div.id = elemento.IDMun;
     div.classList.add("col-8","col-sm-6","col-md-4","col-lg-3","col-xl-2","bg-white","text-dark","card");
-    div.innerHTML +=`<div class="card-body">
+    div.innerHTML +=`<div class="card-body" id="municipiosCarta">
           <h5 class="card-title text-center ">${elemento.nombreMun}</h5>
       </div>`; 
       contenedor.appendChild(div);
